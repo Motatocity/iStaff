b2c655c3faa825f85abb5701abcf068ec1ff1ec8
Motatocity <motatocity@hotmail.com>
2018-08-17 11:41:51 +0700

    update config

--- www/js/app.js
+++ www/js/app.js
@@ -45,10 +45,18 @@ document.addEventListener('deviceready', function () {
   var notificationOpenedCallback = function(jsonData) {
     console.log('notificationOpenedCallback: ' + JSON.stringify(jsonData));
   };
-
+/*
   window.plugins.OneSignal
     .startInit("14b162b3-4125-4f89-af4e-741d0ff9e0f9")
     .handleNotificationOpened(notificationOpenedCallback)
     .endInit();
+*/
+	window.plugins.OneSignal.init("14b162b3-4125-4f89-af4e-741d0ff9e0f9",
+                                 {googleProjectNumber: "294430135080"},
+                                 notificationOpenedCallback);
+
+	// Show an alert box if a notification comes in when the user is in your app.
+	window.plugins.OneSignal.enableInAppAlertNotification(true);
+  
 }, false);
 
