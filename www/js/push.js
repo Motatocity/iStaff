f239b4e1d92297b941812f7d76f5aa3db3c566f4
Motatocity <motatocity@hotmail.com>
2018-10-08 15:16:37 +0700

    update test notif

--- www/js/push.js
+++ www/js/push.js
@@ -1,7 +1,7 @@
 document.addEventListener('deviceready', function () {
   // Enable to debug issues.
   //window.plugins.OneSignal.setLogLevel({logLevel: 4, visualLevel: 4});
-  pushnotif();
+  //pushnotif();
   
   var notificationOpenedCallback = function(jsonData) {
     console.log('notificationOpenedCallback: ' + JSON.stringify(jsonData));
