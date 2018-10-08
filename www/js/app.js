52bc4931a3cdd458f15b92f56e1e73d759db7e83
Motatocity <motatocity@hotmail.com>
2018-10-08 10:40:43 +0700

    test push by onesignal

--- www/js/app.js
+++ www/js/app.js
@@ -35,31 +35,6 @@ function myEventHandler() {
     alert(str) ;
 }
 
-document.addEventListener('deviceready', function () {
-  // Enable to debug issues.
-  window.plugins.OneSignal.setLogLevel(OneSignal.LOG_LEVEL.DEBUG, OneSignal.LOG_LEVEL.DEBUG);
-  
-  var notificationOpenedCallback = function(jsonData) {
-    console.log('notificationOpenedCallback: ' + JSON.stringify(jsonData));
-  };
-
-  window.plugins.OneSignal
-    .startInit("13933cf9-c711-4cc9-8a29-a33cb29041a2")
-    .handleNotificationOpened(notificationOpenedCallback)
-    .endInit();
-
-	/*
-	window.plugins.OneSignal.init("14b162b3-4125-4f89-af4e-741d0ff9e0f9",
-                                 {googleProjectNumber: "294430135080"},
-                                 notificationOpenedCallback);
-	*/
-
-	// Show an alert box if a notification comes in when the user is in your app.
-	//window.plugins.OneSignal.enableInAppAlertNotification(true);
-  
-}, false);
-
-
 // ...additional event handlers here... 
 /*
 document.addEventListener('deviceready', function () {
